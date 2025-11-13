ff5668b922e60105e90d630d3a0c108fff86e7f1
A V I I <kumar.arun.ynr@gmail.com>
2025-11-13 20:49:05 +0530

    Add Settings View.

--- RickAndMorty/Screens/Settings/Models/RMSettingOption.swift
+++ RickAndMorty/Screens/Settings/Models/RMSettingOption.swift
@@ -5,9 +5,12 @@
 //  Created by Arun on 10/07/23.
 //
 
-import UIKit
+import SwiftUI
 
-enum RMSettingOption: CaseIterable {
+enum RMSettingOption: Identifiable, CaseIterable {
+    
+    var id: Self { self }
+    
     case rateApp
     case contactUs
     case terms
@@ -18,78 +21,66 @@ enum RMSettingOption: CaseIterable {
     
     var displayTitle: String {
         switch self {
-        case .rateApp:
-            return "Rate App"
-        case .contactUs:
-            return "Contact Us"
-        case .terms:
-            return "Terms of service"
-        case .privacy:
-            return "Privacy Policy"
-        case .apiReference:
-            return "API Reference"
-        case .viewSeries:
-            return "View Video Series"
-        case .viewCode:
-            return "View App Code"
+        case .rateApp: "Rate App"
+        case .contactUs: "Contact Us"
+        case .terms: "Terms of service"
+        case .privacy: "Privacy Policy"
+        case .apiReference: "API Reference"
+        case .viewSeries: "View Video Series"
+        case .viewCode: "View App Code"
         }
     }
     
-    var displayImage: UIImage? {
+    var systemImageName: String {
         switch self {
-        case .rateApp:
-            return UIImage(systemName: "star.fill")
-        case .contactUs:
-            return UIImage(systemName: "paperplane.fill")
-        case .terms:
-            return UIImage(systemName: "doc.fill")
-        case .privacy:
-            return UIImage(systemName: "lock.fill")
-        case .apiReference:
-            return UIImage(systemName: "list.clipboard.fill")
-        case .viewSeries:
-            return UIImage(systemName: "tv.fill")
-        case .viewCode:
-            return UIImage(systemName: "hammer.fill")
+        case .rateApp: "star.fill"
+        case .contactUs: "paperplane.fill"
+        case .terms: "doc.fill"
+        case .privacy: "lock.fill"
+        case .apiReference: "list.clipboard.fill"
+        case .viewSeries: "tv.fill"
+        case .viewCode: "hammer.fill"
         }
     }
     
-    var imageContainerColor: UIColor {
+    var imageContainerColor: Color {
         switch self {
-        case .rateApp:
-            return .systemPink
-        case .contactUs:
-            return .systemMint
-        case .terms:
-            return .systemGray
-        case .privacy:
-            return .systemBlue
-        case .apiReference:
-            return .systemRed
-        case .viewSeries:
-            return .systemCyan
-        case .viewCode:
-            return .systemTeal
+        case .rateApp: .pink
+        case .contactUs: .mint
+        case .terms: .gray
+        case .privacy: .blue
+        case .apiReference: .red
+        case .viewSeries: .cyan
+        case .viewCode: .teal
         }
     }
-      
+    
     var targetUrl: URL? {
+        
+        let urlString: String?
+        
         switch self {
-        case .rateApp:
-            return nil
         case .contactUs:
-            return URL(string: "https://iosacademy.io")
+            urlString = "https://iosacademy.io"
         case .terms:
-            return URL(string: "https://iosacademy.io/terms")
+            urlString = "https://iosacademy.io/terms"
         case .privacy:
-            return URL(string: "https://iosacademy.io/privacy")
+            urlString = "https://iosacademy.io/privacy"
         case .apiReference:
-            return URL(string: "https://rickandmortyapi.com/documentation/#introduction")
+            urlString = "https://rickandmortyapi.com/documentation/#introduction"
         case .viewSeries:
-            return URL(string: "https://youtube.com/playlist?list=PL5PR3UyfTWvdl4Ya_2veOB6TM16FXuv4y")
+            urlString = "https://youtube.com/playlist?list=PL5PR3UyfTWvdl4Ya_2veOB6TM16FXuv4y"
         case .viewCode:
-            return URL(string: "https://github.com/avii-7/RickAndMorty-Swift")
+            urlString = "https://github.com/avii-7/RickAndMorty-Swift"
+        default:
+            urlString = nil
+        }
+        
+        if let urlString, urlString.isEmpty == false {
+            return URL(string: urlString)
+        }
+        else {
+            return nil
         }
     }
 }
-
